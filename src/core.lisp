36ae8586564ff5fb147ed0ced2859d142faa14f1
Giacomo Ritucci <giacomo.ritucci@email.it>
2009-04-30 00:39:55 +0200

    Rewrite: generics and classes

--- src/core.lisp
+++ src/core.lisp
@@ -33,43 +33,45 @@
 (defpackage :org.altervista.rjack.de-sim.core
   (:nicknames :de-sim.core)
   (:use :common-lisp)
-  (:export :schedule :next :run-event
-	   :identifiable :id-of
-	   :simulated :description-of
-	   :event :time-of :action-of
-	   :world :clock-of :events-of))
+  (:export))
+
 
 ;; OVERVIEW
 
-;; de-sim.core is a very, _very_, simple discrete event simulator.  It
-;; consist of a world that maintains a list of events.  With SCHEDULE
-;; you add an event to this list.  Events can change the world.  Let W
-;; be the world and E the first event of the list: with NEXT you get a
-;; new world W' resulting from the action of E on W.  Note that W and
-;; W' may or may not differ, depending from the action carried by E.
-;;
-;; That's all.  Extend the WORLD class and the EVENT class to
-;; customize the world to your needs.  The SIMULATED class can be used
-;; as the base class of all the simulated objects in the world.
-;;
-;; de-sim.core does not provide a way to cancel scheduled events, you
-;; must choose and implement a strategy yourself.
+;; TODO
 
 
 (in-package :de-sim.core)
 
 
-(defgeneric run-event (event world)
-  (:documentation "Execute event's action giving world as argument."))
+(let ((clock 0))
+
+  (defun gettime ()
+    clock))
 
 
-(defgeneric next (world)
-  (:documentation "Execute the action associated with the next event
-  returning a new world, possibly different from the given one."))
+(defgeneric imminent-event (obj)
+  (:documentation "Returns time, action and owner of the next event to
+  be executed belonging to obj."))
 
 
-(defgeneric schedule (word event)
-  (:documentation "Add event into world's events."))
+(defgeneric notifications (obj)
+  (:documentation "Return the list of the notifications that can be
+  sent to obj."))
+
+
+(defgeneric observable-states (obj)
+  (:documentation "Return the list of obj's observable states."))
+
+
+(defgeneric subscribe (sub obj state notification)
+  (:documentation "After this call sub will be notified with the
+  specified notification when obj will be in the specified state."))
+
+
+(defgeneric unsubscribe (sub obj state)
+  (:documentation "After this call sub will be no more notified when
+  obj will be in the specified state."))
 
 
 (defclass identifiable ()
@@ -78,73 +80,28 @@
     :initform (error ":id missing")
     :accessor id-of
     :type fixnum
-    :documentation "Unique or not, auto-generated or not, subclasses
-    decide.")))
+    :documentation "Unique id")))
 
 
-(defclass simulated (identifiable)
+(defclass object (identifiable)
   ((description
     :initarg :description
     :initform "simulated object"
     :accessor description-of
-    :type string
-    :documentation "Description of this object.")))
-
-
-(defclass event (identifiable)
-  ((time
-    :initarg :time
-    :initform (error "missing :time")
-    :reader time-of
-    :type fixnum
-    :documentation "When this event must be executed.")
-
-   (action
-    :initarg :action
-    :initform (error "missing :action")
-    :reader action-of
-    :type function   ; FIXME: must fully specify signature.
-    :documentation "Implements the behaviour of this event, taking the
-    world as its only argument and modifying it.
-    NOTE: function can modify the world structure only, NOT the
-    structure of world's elements. This is because they are shared
-    with the old instances of world that are kept in the history.")))
-
-
-(defclass world (identifiable)
-  ((clock
-    :initform 0
-    :initarg :clock
-    :accessor clock-of
-    :type fixnum
-    :documentation "World's internal clock.")
-
+    :type string)
    (events
     :initarg :events
     :initform (error "missing :events")
     :accessor events-of
-    :type list
-    :documentation "World's events.")))
+    :type list)))
 
 
-(defmethod schedule ((w world) (ev event))
-  (if (< (time-of ev)
-	 (clock-of w))
-      (error "Scheduling an event in the past.")
-      (setf (events-of w)
-	    (stable-sort (append (events-of w) (list ev))
-			 #'< :key #'time-of))))
+(defclass simulator (object)
+  ((objects
+    :initarg :objects
+    :initform ()
+    :type list)))
 
 
-(defmethod run-event ((ev event) (w world))
-  (funcall (action-of ev) w))
-
-
-(defmethod next ((w world))
-  (with-accessors ((events events-of) (clock clock-of)) w
-    (when (not (null events))
-      (let ((ev (pop events)))
-	(setf clock (time-of ev))
-	(format t "~&~D " clock)
-	(run-event ev w)
-	w))))
+(defclass simulated (object)
+  nil)
