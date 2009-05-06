248b4c5b71903d386f9bf28619598d42fbcc4e7f
Giacomo Ritucci <giacomo.ritucci@email.it>
2009-05-06 14:10:38 +0200

    Util unit test, incomplete

---
+++ src/test.util.lisp
@@ -0,0 +1,56 @@
+;; DE-SIM
+;; Discrete Event SIMulator.
+
+;; Copyright (C) 2009  Giacomo Ritucci
+
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+;;
+;;   1. Redistributions of source code must retain the above copyright
+;;      notice, this list of conditions and the following disclaimer.
+;;   2. Redistributions in binary form must reproduce the above
+;;      copyright notice, this list of conditions and the following
+;;      disclaimer in the documentation and/or other materials
+;;      provided with the distribution.
+;;
+;; THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+;; WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+;; OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+;; INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+;; STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+;; OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+(declaim (optimize debug safety (speed 0)))
+
+
+(defpackage :org.altervista.rjack.de-sim.test.util
+  (:nicknames :de-sim.test.util)
+  (:use :common-lisp :lisp-unit :de-sim.core :de-sim.util))
+
+
+(in-package :de-sim.test.util)
+
+
+(define-test collect-list
+  (let* ((size (random 1000))
+	 (lst (collect-list size (lambda ()
+				   (make-instance 'object)))))
+    (assert-eql size (length lst))))
+
+
+(define-test schedule
+  ;; must raise an error with negative delay
+  ;; must insert events in order
+  ;; must find what have inserted
+  ;; length must be consistent with number of insertions
+  ;; args-of must be unbound if unspecified as parameter of schedule
+  (assert-true nil "TODO"))
+
+(run-tests)
